3d1d52edaaae5379d294b4d0221054884d11f873
Ethan McCreadie <ethanmcc@gmail.com>
2016-01-18 14:42:15 -0500

    v0.17

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='weatherman',
-    version='0.16',
+    version='0.17',
     description='Elastic Beanstalk Stack Launcher',
     author='Ethan McCreadie',
     author_email='ethanmcc@gmail.com',
